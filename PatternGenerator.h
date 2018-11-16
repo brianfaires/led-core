1a12994276a58e9492bc25681124a7d7acb3e94c
Faires, Brian <brianfaires@gmail.com>
2018-11-15 22:07:32 -0800

    Named patterns, added Pattern macros and exposed bright/trans factors

--- PatternGenerator.h
+++ PatternGenerator.h
@@ -9,36 +9,40 @@
 #define NUM_DIM_PATTERNS 16
 #define NUM_COLOR_PATTERNS 2
 
+enum dim_pattern_names { COMET_F, COMET_R, TWO_SIDED, BARBELL, SLOPED_TOWERS_H, SLOPED_TOWERS_L, SLIDE_H, SLIDE_L, BOWTIES_F, BOWTIES_R, TOWERS, SNAKE, SNAKE2, THREE_COMETS_F, THREE_COMETS_R};
+
 class PatternGenerator {
   public:
     uint8_t numColors, brightLength, transLength, dimPeriod, colorPeriod;
     
     PatternGenerator();
     void Init(PaletteManager* _pm, GammaManager* gm);
-	void WriteDimPattern(uint8_t targetDimPatternIndex, uint8_t* outputArray);
-    void WriteColorPattern(uint8_t targetColorPatternIndex, CRGB* outputArray);
+	void WriteDimPattern(uint8_t patternIndex, uint8_t* outputArray);
+    void WriteColorPattern(uint8_t patternIndex, CRGB* outputArray);
+	uint8_t GetTransFactor(uint8_t patternIndex);
+	uint8_t GetBrightFactor(uint8_t patternIndex);
 
   private:
 	PaletteManager* pm;
 	GammaManager* Gamma;
 	
-    void WriteColorPattern_Gradient(CRGB* outputPattern);
-    void WriteColorPattern_Blocks(CRGB* outputPattern);
+    void WriteColorPattern_Gradient(CRGB* outputArray);
+    void WriteColorPattern_Blocks(CRGB* outputArray);
     
-    void WriteDimPattern_Comet(uint8_t* outputPattern);
-    void WriteDimPattern_ReverseComet(uint8_t* outputPattern);
-    void WriteDimPattern_TwoSided(uint8_t* outputPattern);
-    void WriteDimPattern_Barbell(uint8_t* outputPattern);
-    void WriteDimPattern_Snake(uint8_t* outputPattern);
-    void WriteDimPattern_Snake2(uint8_t* outputPattern);
-    void WriteDimPattern_ThreeEvenComets(uint8_t* outputPattern);
-    void WriteDimPattern_ThreeEvenReverseComets(uint8_t* outputPattern);
-    void WriteDimPattern_Towers(uint8_t* outputPattern);
-    void WriteDimPattern_Bowties(uint8_t* outputPattern);
-    void WriteDimPattern_ReverseBowties(uint8_t* outputPattern);
-    void WriteDimPattern_SlopedHighTowers(uint8_t* outputPattern);
-    void WriteDimPattern_SlopedLowTowers(uint8_t* outputPattern);
-    void WriteDimPattern_SlideHigh(uint8_t* outputPattern);
-    void WriteDimPattern_SlideLow(uint8_t* outputPattern);
-    void WriteDimPattern_NoDim(uint8_t* outputPattern);
+    void WriteDimPattern_Comet(uint8_t* outputArray);
+    void WriteDimPattern_ReverseComet(uint8_t* outputArray);
+    void WriteDimPattern_TwoSided(uint8_t* outputArray);
+    void WriteDimPattern_Barbell(uint8_t* outputArray);
+    void WriteDimPattern_Snake(uint8_t* outputArray);
+    void WriteDimPattern_Snake2(uint8_t* outputArray);
+    void WriteDimPattern_ThreeEvenComets(uint8_t* outputArray);
+    void WriteDimPattern_ThreeEvenReverseComets(uint8_t* outputArray);
+    void WriteDimPattern_Towers(uint8_t* outputArray);
+    void WriteDimPattern_Bowties(uint8_t* outputArray);
+    void WriteDimPattern_ReverseBowties(uint8_t* outputArray);
+    void WriteDimPattern_SlopedHighTowers(uint8_t* outputArray);
+    void WriteDimPattern_SlopedLowTowers(uint8_t* outputArray);
+    void WriteDimPattern_SlideHigh(uint8_t* outputArray);
+    void WriteDimPattern_SlideLow(uint8_t* outputArray);
+    void WriteDimPattern_NoDim(uint8_t* outputArray);
 };
